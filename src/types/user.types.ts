3e83e011afe1ac699315b2c9d907df494c20014b
ftodoric <fptodoric@gmail.com>
2023-04-09 01:07:17 +0200

    Tidy up types

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -1,13 +1,6 @@
-export enum UserRole {
-  STUDENT = 'student',
-  TUTOR = 'tutor',
-  ADMIN = 'admin',
-}
-
 export type User = {
   id: number
   username: string
-  // role: UserRole;
 }
 
 export type OptionalUser = User | null
