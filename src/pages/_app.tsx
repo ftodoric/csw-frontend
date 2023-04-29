0db804a8e66f4a4f195ad012b675447021885a37
ftodoric <fptodoric@gmail.com>
2023-04-29 14:01:32 +0200

    Transfer font link to document page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,13 +16,6 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
 
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-
         <meta
           name="description"
           content="Simulate a cyber security warfare situation between two opposing sides each representing a nation of their own with a government and four more appropriate entites."
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,7 +33,15 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+
         <body>
           <Main />
           <NextScript />
