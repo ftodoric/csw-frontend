306a07b2bec7107b8680777f6fb2c3f881cd2b89
ftodoric <fptodoric@gmail.com>
2023-06-06 22:54:57 +0200

    Set public server domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const DEV_DOMAIN = 'localhost:8000'
-const PROD_DOMAIN = 'www.ftodoric.com'
+const PROD_DOMAIN = '134.209.254.186/8000'
 
 const DEV_API_BASE_URL = `http://${DEV_DOMAIN}/api`
 const PROD_API_BASE_URL = `https://${PROD_DOMAIN}/api`
