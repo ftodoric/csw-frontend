d48726676ce8bd0ff8a912aafc19bc18d5e2f531
ftodoric <fptodoric@gmail.com>
2023-06-07 18:45:27 +0200

    Revert to ws endpoint

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const DEV_API_BASE_URL = `http://${DEV_DOMAIN}/api`
 const PROD_API_BASE_URL = `https://${PROD_DOMAIN}/api`
 
 const DEV_WS_URL = `ws://${DEV_DOMAIN}`
-const PROD_WS_URL = `wss://${PROD_DOMAIN}`
+const PROD_WS_URL = `ws://${PROD_DOMAIN}`
 
 const nextConfig = {
   reactStrictMode: true,
