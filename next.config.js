30b1557db88c02c1e572033c20b656414955e613
ftodoric <fptodoric@gmail.com>
2023-06-07 17:53:27 +0200

    Adjust to new https prod api endpoint

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const DEV_DOMAIN = 'localhost:8000'
-const PROD_DOMAIN = '134.209.254.186:8000'
+const PROD_DOMAIN = 'api.cs-wargame.com'
 
 const DEV_API_BASE_URL = `http://${DEV_DOMAIN}/api`
-const PROD_API_BASE_URL = `http://${PROD_DOMAIN}/api`
+const PROD_API_BASE_URL = `https://${PROD_DOMAIN}/api`
 
 const DEV_WS_URL = `ws://${DEV_DOMAIN}`
 const PROD_WS_URL = `ws://${PROD_DOMAIN}`
